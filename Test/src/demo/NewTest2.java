32d419095f7ce5e9ef18d3f53abe6c6add31ea21
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2019-07-18 12:35:15 +0530

    Update NewTest2.java

--- Test/src/demo/NewTest2.java
+++ Test/src/demo/NewTest2.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 public class NewTest2 {
 	WebDriver driver;
 
-	@Test(groups = { "Regression" })
+	@Test(priority=0,groups = { "Regression" })
 	public void loginToApplication() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\VSPICT\\Downloads\\chromedriver.exe");
 		driver = new ChromeDriver();
@@ -21,23 +21,22 @@ public class NewTest2 {
 		driver.findElement(By.id("btnLogin")).click();
 }
 
-	@Test(groups = "Smoke")
+	@Test(priority=1,groups= {"Regression"})
 	public void searchForUser() throws InterruptedException {
 		driver.findElement(By.xpath("//b[contains(text(),'Admin')]")).click();
-
-		// driver.findElement(By.className("searchbutton123")).click();
+		driver.findElement(By.id("searchSystemUser_userName")).sendKeys("vbtest");
+		driver.findElement(By.className("searchbutton")).click();
 		System.out.println("S1");
 
 	}
 
-	@Test(groups = "Smoke")
+	@Test(priority=2,groups = "Regression")
 	public void deleteTheUser() throws InterruptedException {
-		// driver.findElement(By.xpath("//tr[@class='odd']/td/input[@type='checkbox']")).click();
+		driver.findElement(By.xpath("//tr[@class='odd']/td/input[@type='checkbox']")).click();
 		// find delete button by id
-		// driver.findElement(By.id("btnDelete")).click();
+		//driver.findElement(By.id("btnDelete")).click();
 		// find delete button by xpath
-		// driver.findElement(By.xpath("//div[@class='top']/input[@type='submit' and
-		// @class='delete']")).click();
+		//driver.findElement(By.xpath("//div[@class='top']/input[@type='submit' and @class='delete']")).click();
 		System.out.println("S2");
 
 	}
